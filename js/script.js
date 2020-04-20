b2cca638e071b3cce13684d352c04a91ad4fe0a5
wyssalkl <wisslklebi1@gmail.com>
2020-04-20 18:24:03 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function($){
+    var typed = new Typed('span.txt-rotate', {
+        strings:["I'm a Data Entry Clerk.","I'm a Transcriptionist.", "I'm a Translator."],
+        typeSpeed:80,
+        backSpeed:50,
+        fadeOut:false,
+        smartBackspace:true,
+        loop:true
+    });
+
+
+})(jQuery);
